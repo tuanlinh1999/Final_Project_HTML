8a065d445de657b02c290031d8e8f9709ed095a1
tuanlinh1999 <59931178+tuanlinh1999@users.noreply.github.com>
2021-04-13 11:05:11 -0400

    s

--- Group_Final_Project/script/script.js
+++ Group_Final_Project/script/script.js
@@ -1,13 +1,12 @@
-window.onload = function()
-{
-            document.getElementById('toggle').onclick = function(){
-                this.previousElementSibling.classList.toggle('show');
-            }
-            document.getElementById('close').onclick = function(){
-                document.getElementById('overlay').classList.toggle('showoverlay');
-            }
-            
-            document.getElementById('registration').onclick = function(){
-                document.getElementById('overlay').classList.toggle('showoverlay');
-            }
+window.onload = function() {
+    document.getElementById('toggle').onclick = function() {
+        this.previousElementSibling.classList.toggle('show');
+    }
+    document.getElementById('close').onclick = function() {
+        document.getElementById('overlay').classList.toggle('showoverlay');
+    }
+
+    document.getElementById('registration').onclick = function() {
+        document.getElementById('overlay').classList.toggle('showoverlay');
+    }
 };
