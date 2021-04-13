33f23f7d8e238f54800ebd2544ad923f26231f98
shivam269807 <80052677+shivam269807@users.noreply.github.com>
2021-04-13 20:18:46 +0530

    done

---
+++ Group_Final_Project/script/script.js
@@ -0,0 +1,13 @@
+window.onload = function()
+{
+            document.getElementById('toggle').onclick = function(){
+                this.previousElementSibling.classList.toggle('show');
+            }
+            document.getElementById('close').onclick = function(){
+                document.getElementById('overlay').classList.toggle('showoverlay');
+            }
+            
+            document.getElementById('registration').onclick = function(){
+                document.getElementById('overlay').classList.toggle('showoverlay');
+            }
+};
